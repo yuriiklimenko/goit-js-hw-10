5e915eeff773b1ab70d673246636c7c4a04e8a3a
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2022-10-16 10:46:16 +0300

    -init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', e => {
+  fetchCountry(e.currentTarget.value).then(countries => {
+    // console.log(countries);
+    renderMarkupCounter(countries);
+  });
+});
+
+function fetchCountry(country) {
+  return fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    })
+    .catch(error => {
+      console.log('SuperError!!!', error);
+    });
+}
+
+function renderMarkupCounter(countries) {
+  const markup = countries
+    .map(({ flags, name }) => {
+      return `<li>
+                <img class="flag" src="${flags.svg}" alt="flag">
+                 <p>${name.common}</p>
+            </li>`;
+    })
+    .join('');
+
+  countryList.innerHTML = markup;
+}
